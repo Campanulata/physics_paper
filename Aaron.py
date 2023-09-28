1c4b9c8d85befa60cecc159fa44ce00e5b87e41d
Campanulata <t.512@outlook.com>
2023-09-28 15:48:31 +0800

    test

--- Aaron.py
+++ Aaron.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import re
 
+
 class Aaron:
     def __init__(self, input_file, output_file):
         self.input_file = input_file
         self.output_file = output_file
 
-    def determine_question_type(question):
+    def determine_question_type(self, question):
         if re.search(r'\{.*?\}', question):  # 填空题判断，检查是否有{}
             return "填空题"
         elif re.search(r'[A-D]\.', question):  # 选择题判断，检查是否有A.、B.、C.、D.
@@ -26,12 +27,13 @@ class Aaron:
         questions = markdown_data.split('\n\n')
 
         # 创建一个DataFrame来存储选择题
-        df = pd.DataFrame(columns=['题干', '选项A', '选项B', '选项C', '选项D', '答案', '详解'])
+        df = pd.DataFrame(
+            columns=['题干', '选项A', '选项B', '选项C', '选项D', '答案', '详解'])
 
         # 遍历每个选择题
         for question in questions:
             # 获取题型
-            question_type=self.determine_question_type(question)
+            question_type = self.determine_question_type(question)
             match question_type:
                 # 填空题
                 case '填空题':
@@ -43,18 +45,19 @@ class Aaron:
                     # 使用正则表达式进行匹配
                     match = re.search(pattern, question, re.DOTALL)
                     # 提取匹配的变量，微调
-                    data_df=pd.DataFrame({
-                            '题干': re.sub(r"^[0-9]+\.", "", match.group("题干")).strip(),  # 去除题干的序号
-                            '选项A': match.group("选项A")[3:],  # 去除选项开头的字母和点,
-                            '选项B': match.group("选项B")[3:],
-                            '选项C': match.group("选项C")[3:],
-                            '选项D': match.group("选项D")[3:],
-                            '答案': match.group("答案"),
-                            '详解': match.group("详解")
-                        }, index=[0])
+                    data_df = pd.DataFrame({
+                        # 去除题干的序号
+                        '题干': re.sub(r"^[0-9]+\.", "", match.group("题干")).strip(),
+                        '选项A': match.group("选项A")[3:],  # 去除选项开头的字母和点,
+                        '选项B': match.group("选项B")[3:],
+                        '选项C': match.group("选项C")[3:],
+                        '选项D': match.group("选项D")[3:],
+                        '答案': match.group("答案"),
+                        '详解': match.group("详解")
+                    }, index=[0])
                     # 将提取的数据添加到DataFrame中
                     if match:
-                        df = pd.concat([df,data_df], ignore_index=True)
+                        df = pd.concat([df, data_df], ignore_index=True)
                     else:
                         print("未找到匹配")
         # 统计查询到的题目数量
@@ -65,6 +68,7 @@ class Aaron:
 
         # 保存为CSV文件
         df.to_csv('output.csv', index=False)
+
 
 # 示例用法
 aaron = Aaron('input.txt', 'output.csv')
